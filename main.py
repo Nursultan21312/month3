29f3fb0eed21d50e453788c139843e0851a9a383
Nursultan21312 <ilihom0330@mail.ru>
2025-11-06 21:03:24 +0600

    update:main.py

--- main.py
+++ main.py
@@ -2,33 +2,37 @@ import flet as ft
 
 def main_page(page:ft.Page):
     page.title = 'Мое первое приложение'
-    greeting_text = ft.Text(value='hello world')
+    greeting_text = ft.Text(value='Привет, как тебя зовут и сколько тебе лет?')
 
 
 
     def on_button_click(_):
         name = name_input.value.strip()
+        age = age_input.value.strip()
 
         print(greeting_text.value)
-        greeting_text.value = f'hello{name}'
+        greeting_text.value = f'Привет{name} , тебе {age} лет'
         print(greeting_text.value)
 
-        if name:
+        if age:
             greeting_text.color = None
-            greeting_text.value = f'hello {name}'
-            name_input.value = None
+            greeting_text.value = f'Привет {name} , тебе {age} лет'
+            age_input.value = None
 
         else:
-            greeting_text.value = 'error'
-            greeting_text.color = ft.Colors.RED
+            greeting_text.value = 'Введите возраст!'
+            greeting_text.color = ft.Colors.RED    
+
+        
 
         page.update()
 
             
     name_input = ft.TextField(label='введите имя' , on_submit=on_button_click)
+    age_input = ft.TextField(label='введите возраст' , on_submit=on_button_click)
     input_button_text = ft.TextButton(text='send', icon =ft.Icons.SEND_ROUNDED , on_click=on_button_click)
 
-    page.add(greeting_text , name_input , input_button_text)
+    page.add(greeting_text , name_input ,age_input, input_button_text)
 
 
     
